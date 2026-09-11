d580450809989fcfddeb8ac790b88fc3c5c85efe
Gentianahaliti <gentianajasharII@outlook.com>
2026-09-11 13:31:04 +0200

    Lägger till början

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;  
+// Det här är som att öppna en verktygslåda som innehåller allt grundläggande i C#.
+// Utan den kan jag inte skriva ut text eller läsa in text från användaren.
+
+using System.Collections.Generic; 
+// Detta öppnar en extra verktygslåda som innehåller Listor.
+// Listor är som flexibla lådor där jag kan lägga in saker och ta bort saker.
+// Detta behöver jag för att kunna skapa List<string> och List<int>.
+
+class Program  
+// En klass är som en behållare där jag lägger all kod som hör ihop.
+// Här ligger hela mitt program.
+{
+    static void Main()  
+    // Detta är startpunkten. När jag kör programmet börjar C# här.
+    {
+    }
+}
